5601c381e4ebf9011556387306e33d59564f1e02
Shubham Sharma <shubh.luck@yahoo.in>
2025-07-11 14:42:56 -0400

    ODP-4589 Fix Timestamp related issue

--- src/java/org/apache/sqoop/mapreduce/hcat/SqoopHCatExportHelper.java
+++ src/java/org/apache/sqoop/mapreduce/hcat/SqoopHCatExportHelper.java
@@ -108,9 +108,6 @@ public class SqoopHCatExportHelper {
         + recordClassName);
     }
 
-    //String inputJobInfoStr = conf.get(HCatConstants.HCAT_KEY_JOB_INFO);
-    //jobInfo =
-     // (InputJobInfo) HCatUtil.deserialize(inputJobInfoStr);
     jobInfo = HCatUtil.getLastInputJobInfosFromConf(conf);
     HCatSchema tableSchema = jobInfo.getTableInfo().getDataColumns();
     HCatSchema partitionSchema =
